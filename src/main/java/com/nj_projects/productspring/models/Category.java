8d9741d0f32facb5f44d3e2e96aeb2c373001cae
Manan21st <manan9346@gmail.com>
2024-04-13 18:28:07 +0530

    This implements a local MySQL database and minor constructor changes.

--- src/main/java/com/nj_projects/productspring/models/Category.java
+++ src/main/java/com/nj_projects/productspring/models/Category.java
@@ -1,11 +1,19 @@
 package com.nj_projects.productspring.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Category {
+    @Id
     private Long id;
     private String name;
 }
--- src/main/java/com/nj_projects/productspring/models/Product.java
+++ src/main/java/com/nj_projects/productspring/models/Product.java
@@ -1,15 +1,26 @@
 package com.nj_projects.productspring.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Product {
+    @Id
     private Long id;
     private String title;
     private double price;
     private String description;
+    @ManyToOne
     private Category category;
     private String imageUrl;
 }
